0bb411b1287acd8e1bfbc4189c4e2e7bbc6094a0
CuppaKe <turnman87@gmail.com>
2025-04-12 13:47:02 +0400

    feat: add docke and eb

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-
-import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +13,6 @@ async function bootstrap() {
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
-  //   app.use(helmet());
 
   await app.listen(port, () => {
     console.log('App is running on %s port', port);
